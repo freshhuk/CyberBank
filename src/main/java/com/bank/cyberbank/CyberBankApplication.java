14dd216050b7083b3df9c45bf106884d44d4e52b
freshhuk <freshhuk13@gmail.com>
2024-01-06 22:49:29 +0200

    init

---
+++ src/main/java/com/bank/cyberbank/CyberBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.cyberbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/cyberbank/CyberBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.cyberbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
