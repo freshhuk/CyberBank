9b83dc0a6db3b97033012bd61b189ed80c1dfa6a
freshhuk <freshhuk13@gmail.com>
2024-01-18 23:47:59 +0200

    Some add

--- src/main/java/com/bank/cyberbank/Controllers/UserController.java
+++ src/main/java/com/bank/cyberbank/Controllers/UserController.java
@@ -1,15 +1,17 @@
 package com.bank.cyberbank.Controllers;
 
 
-
-
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
 
 This class for end point for user
 
  */
-
+@RestController
+@RequestMapping("/api")
 public class UserController
 {
 }
--- src/main/java/com/bank/cyberbank/Services/BankCardService.java
+++ src/main/java/com/bank/cyberbank/Services/BankCardService.java
@@ -1,7 +1,18 @@
 package com.bank.cyberbank.Services;
 
+import com.bank.cyberbank.Repositories.BankCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BankCardService
 {
+    private final BankCardRepository bankCardRepository;
+    @Autowired
+    public BankCardService(BankCardRepository bankCardRepository)
+    {
+        this.bankCardRepository = bankCardRepository;
+    }
     //Method for creating bankcard for user
     public void CreateBankCard()
     {
