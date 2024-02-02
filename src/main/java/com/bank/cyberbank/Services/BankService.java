47ffaf0ac785a036a8c1da9ce1ced5e92ee4e9c5
freshhuk <freshhuk13@gmail.com>
2024-02-02 12:03:53 +0200

    Method LoadOwnMoney done

--- src/main/java/com/bank/cyberbank/Services/BankService.java
+++ src/main/java/com/bank/cyberbank/Services/BankService.java
@@ -1,11 +1,37 @@
 package com.bank.cyberbank.Services;
 
+import com.bank.cyberbank.Repositories.BankCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BankService
 {
-    //Method for loading your money in your bank card
-    public void LoadOwnMoney()
-    {
 
+    private final BankCardRepository repository;
+    @Autowired
+    public BankService(BankCardRepository repository)
+    {
+        this.repository = repository;
+    }
+    //Method for loading your money in your bank card
+    public String LoadOwnMoney(String numberBankCard, int loadMoney)
+    {
+        try{
+            var bankCard = repository.GetBankCardByNumber(numberBankCard);
+            if(bankCard != null)
+            {
+                int newCardMoney = bankCard.getBalance() + loadMoney;
+                bankCard.setBalance(newCardMoney);
+                return "Successful";
+            }
+            return "Bank card not found";
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Exception - " + ex);
+            return "Error";
+        }
     }
     //Method for Withdraw money from your bank card
     public void WithdrawMoneyFromCard()
@@ -23,7 +49,7 @@ public class BankService
     {
 
     }
-    
+
 
 
 }
