e5186ef9d3383088a5abc6b3dee18ae4d31f6fa8
freshhuk <freshhuk13@gmail.com>
2024-02-27 23:05:11 +0200

    Created new admin methed

--- src/main/java/com/bank/cyberbank/Controllers/AdminController.java
+++ src/main/java/com/bank/cyberbank/Controllers/AdminController.java
@@ -1,5 +1,15 @@
 package com.bank.cyberbank.Controllers;
 
+import com.bank.cyberbank.Domain.Entity.BankCard;
+import com.bank.cyberbank.Services.BankCardService;
+import com.bank.cyberbank.Services.BankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /*
@@ -7,12 +17,30 @@ package com.bank.cyberbank.Controllers;
 This class for end point for user
 
  */
-
-
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController
 {
-    public void CheckAllUserInfo()
+    private final BankService bankService;
+    private final BankCardService bankCardService;
+
+    @Autowired
+    public AdminController(BankService bankService, BankCardService bankCardService)
     {
+        this.bankService = bankService;
+        this.bankCardService = bankCardService;
+    }
+
+    @GetMapping("/getAllUser")
+    public ResponseEntity<List<BankCard>> checkAllUserInfo()
+    {
+        try{
+            List<BankCard> cards =  bankCardService.GetAllCard();
+            return ResponseEntity.ok().body(cards);
+        }
+        catch(Exception ex){
+            return ResponseEntity.badRequest().body(null);
+        }
 
     }
     public void deleteUser()
