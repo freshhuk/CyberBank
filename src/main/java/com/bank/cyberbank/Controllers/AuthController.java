874991cde9d0b885a26db149f2c01f42b04117f8
freshhuk <freshhuk13@gmail.com>
2024-06-15 22:57:31 +0300

    some add

--- src/main/java/com/bank/cyberbank/Controllers/AuthController.java
+++ src/main/java/com/bank/cyberbank/Controllers/AuthController.java
@@ -3,16 +3,14 @@ package com.bank.cyberbank.Controllers;
 import com.bank.cyberbank.Services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * End point for aoth methods
  * Login, register, logout
  */
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
